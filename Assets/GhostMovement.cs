086b752fb702379c6c465bede7fa1be1bf146669
fran516 <rzq512@york.ac.uk>
2025-11-25 15:46:14 +0000

    25/11/25

---
+++ Assets/GhostMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GhostMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       NavMeshAgent agent = GetComponent<NavMeshAgent>();
+       if (agent.remainingDistance <= 1.0f) {
+        float x = Random.Range(-20.0f,20.0f);
+        float z = Random.Range(-20.0f,20.0f);
+        agent.destination = new Vector3(x, 0.0f, z);
+       }
+    }
+}
