79b87de66a9c47708acb21ffb82580b0f760988a
fran516 <rzq512@york.ac.uk>
2025-11-26 10:50:55 +0000

    26/11/25

--- Assets/GhostMovement.cs
+++ Assets/GhostMovement.cs
@@ -3,6 +3,16 @@ using UnityEngine.AI;
 
 public class GhostMovement : MonoBehaviour
 {
+    enum GhostState {
+        WANDERING, 
+        PLAYER_SEEN,
+        PLAYER_CAUGHT,
+        PLAYER_EATS_CHERRY
+    };
+    
+    GhostState state = GhostState.WANDERING;
+    public GameObject player;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,11 +22,32 @@ public class GhostMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       NavMeshAgent agent = GetComponent<NavMeshAgent>();
-       if (agent.remainingDistance <= 1.0f) {
-        float x = Random.Range(-20.0f,20.0f);
-        float z = Random.Range(-20.0f,20.0f);
-        agent.destination = new Vector3(x, 0.0f, z);
-       }
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        if (state == GhostState.WANDERING) {
+            if (agent.remainingDistance <= 1.0f) {
+                float x = Random.Range(-75.0f,75.0f);
+                float z = Random.Range(-75.0f,75.0f);
+                agent.destination = new Vector3(x, 0.0f, z);
+            }
+        } else if (state == GhostState.PLAYER_SEEN) {
+           agent.destination = player.transform.position;
+        } else if (state == GhostState.PLAYER_CAUGHT) {
+            Debug.Log("hello?");
+            //Game over UI
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "Player") {
+            state = GhostState.PLAYER_SEEN;
+            player = other.gameObject;
+        }
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.name == "Player") {
+            state = GhostState.PLAYER_CAUGHT;
+            player = other.gameObject;
+        }
     }
 }
